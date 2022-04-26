e45ff23913c0bee6b40cbd4cbad997e40d27b1e0
chrisallo <chrisallo.h@gmail.com>
2022-04-27 06:25:56 +0900

    add maxheap test

--- src/maxheap.js
+++ src/maxheap.js
@@ -38,4 +38,7 @@ export default class MaxHeap {
     }
     return null;
   }
+  clear() {
+    this.heap = [null];
+  }
 }
--- test/maxheap.test.js
+++ test/maxheap.test.js
@@ -5,4 +5,21 @@ describe('maxheap', () => {
   test('new', () => {
     new MaxHeap();
   });
+  test('push()', () => {
+    const list = [10,5,3,8,14,22];
+    const maxheap = new MaxHeap();
+    list.forEach(n => maxheap.push(n));
+    expect(maxheap.heap).toStrictEqual([null,22,10,14,5,8,3]);
+    expect(maxheap.peek()).toBe(22);
+  });
+  test('push() > pop()', () => {
+    const list = [10,5,3,8,14,22];
+    const maxheap = new MaxHeap();
+    list.forEach(n => maxheap.push(n));
+
+    expect(maxheap.pop()).toBe(22);
+    expect(maxheap.pop()).toBe(14);
+    expect(maxheap.heap).toStrictEqual([null,10,8,3,5]);
+    expect(maxheap.peek()).toBe(10);
+  });
 });
